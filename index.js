4f6d3be7fa91f2aa4de1e47167c056290b5fb51e
JinreP <subeegod@gmail.com>
2025-10-20 19:58:49 +0800

    added male students part 1

--- index.js
+++ index.js
@@ -32,24 +32,42 @@ app.get("/", async (request, response) => {
 
 const students = [];
 
-app.get("/students", async (request, response) => {
-  let studentDetail = {
-    name: "Subeedei",
-    age: 19,
-    Hobby: "video game",
-  };
+app.get("/students/male", async (request, response) => {
+  const maleStudents = students.filter((student) => student.gender === "male");
+  response.send(maleStudents);
+});
 
+app.get("/students", async (request, response) => {
   response.send(students);
 });
 
 app.post("/students", async (req, res) => {
   console.log(req.body);
-  const before = students.filter((student) => student.name === req.body.name);
+  const before = students.filter((student) => student.phone === req.body.phone);
 
   console.log("BEFORE", before);
   if (before.length === 0) {
     students.push(req.body);
     res.status(200).send(students).end();
+  } else {
+    res.status(409).send({ message: "error " });
+  }
+});
+
+app.post("/students/male", async (req, res) => {
+  console.log(req.body);
+  const before = students.filter((student) => student.phone === req.body.phone);
+
+  console.log("BEFORE", before);
+  if (before.length === 0) {
+    if (req.body.gender === "male") {
+      students.push(req.body);
+      res.status(200).send(students).end();
+    } else {
+      res
+        .status(400)
+        .send({ message: "Only male students can be added to this endpoint" });
+    }
   } else {
     res.status(409).send({ message: "error " });
   }
